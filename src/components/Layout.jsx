1a7c85ace606a9f5f9b5caf15fd3a9ea4b44e20e
Pradeep <pradeep23oct@gmail.com>
2022-02-08 20:12:43 +0530

    socials added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,10 +8,28 @@ const cols = 50;
 export default function layout() {
   const [ssData, dispatch] = useSpreadsheet(rows, cols);
   return (
-    <div>
+    <div className="">
       <Header ssData={ssData} dispatch={dispatch}/>
       <div className="header-spacer"></div>
-        <Spreadsheet rows={rows} cols={cols} data={ssData} dispatch={dispatch} />
+      <Spreadsheet rows={rows} cols={cols} data={ssData} dispatch={dispatch} />
+      <footer>
+        <span className="creator">&copy;Pradeep Mishra</span>
+        <span>
+          <a target="_blank" href="https://github.com/pradeep-mishra">
+            <img src="/github.png" alt="github" />
+          </a>
+        </span>
+        <span>
+          <a target="_blank" href="https://www.linkedin.com/in/ipradeepmishra/">
+            <img src="/linkedin.png" alt="linkedin" />
+          </a>
+        </span>
+        <span>
+          <a target="_blank" href="https://twitter.com/ipradeepmishra">
+            <img src="/twitter.png" alt="twitter" />
+          </a>
+        </span>
+      </footer>
     </div>
   );
 }
