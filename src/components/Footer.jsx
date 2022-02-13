74850203c2dfacdc31fc20f7548c6845207c0d24
Pradeep <pradeep23oct@gmail.com>
2022-02-13 17:25:27 +0530

    postscript notice moved to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,38 @@
 
 export default function Footer() {
   return (
-    <footer>
-      <span className="creator">&copy;Pradeep Mishra</span>
-      <span>
-        <a target="_blank" href="https://github.com/pradeep-mishra/grid">
-          <img src="/github.png" alt="github" />
-        </a>
-      </span>
-      <span>
-        <a target="_blank" href="https://www.linkedin.com/in/ipradeepmishra/">
-          <img src="/linkedin.png" alt="linkedin" />
-        </a>
-      </span>
-      <span>
-        <a target="_blank" href="https://twitter.com/ipradeepmishra">
-          <img src="/twitter.png" alt="twitter" />
-        </a>
-      </span>
-    </footer>
+    <>
+      <footer>
+        <span className="creator">&copy;Pradeep Mishra</span>
+        <span>
+          <a target="_blank" href="https://github.com/pradeep-mishra/grid">
+            <img src="/github.png" alt="github" />
+          </a>
+        </span>
+        <span>
+          <a target="_blank" href="https://www.linkedin.com/in/ipradeepmishra/">
+            <img src="/linkedin.png" alt="linkedin" />
+          </a>
+        </span>
+        <span>
+          <a target="_blank" href="https://twitter.com/ipradeepmishra">
+            <img src="/twitter.png" alt="twitter" />
+          </a>
+        </span>
+      </footer>
+      <div class="post-script">
+        <span>
+          Following functions are supported
+        </span>
+        <ol>
+          <li>
+          =sum(1, 2, 3, A2, B5, C2)
+        </li>
+        <li>
+          =eval(js code)
+        </li>
+        </ol>
+      </div>
+    </>
   )
 }
